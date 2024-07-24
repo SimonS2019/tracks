ae2d1aad1aa5398aba4602d8dc978d29e75c8f1b
SimonS2019 <shentiexiong@gmail.com>
2024-07-25 11:11:16 +1200

    Fix a bug: When following a tutorial, we encountered an error with a deprecated Text element in react-native-elements. Switching to React Native's native Text component resolved all errors.

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text, View, StyleSheet } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
       <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
+        <Text style={styles.h3}>Sign Up for Tracker</Text>
       </Spacer>
       <Input label="Email" />
       <Spacer />
@@ -19,6 +19,10 @@ const SignupScreen = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+  h3: {
+    fontSize: 18, // This is an example size; adjust as needed
+    fontWeight: 'bold',
+  },
+});
 export default SignupScreen;
