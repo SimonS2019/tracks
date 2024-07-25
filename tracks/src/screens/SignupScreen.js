6a16885f9de5f58446f7c519410ffe112564ed39
SimonS2019 <shentiexiong@gmail.com>
2024-07-25 16:00:33 +1200

    213. Handling Errored Requests

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -30,6 +30,9 @@ const SignupScreen = ({ navigation }) => {
         autoCapitalize="none"
         autoCorrect={false}
       />
+        {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
       <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
@@ -52,6 +55,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15,
   },
 });
 export default SignupScreen;
