efe89864cf3c7ebed4161189c50597e777871e6b
SimonS2019 <shentiexiong@gmail.com>
2024-07-26 15:49:11 +1200

    250. Extracting Logic to a Hook

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -15,35 +15,13 @@ import {
 } from "expo-location";
 import "../_mockLocation";
 import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
+  const [err] = useLocation(addLocation);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-    } catch (e) {
-      setErr(e);
-    }
-  };
 
-  useEffect(() => {
-    startWatching();
-  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
