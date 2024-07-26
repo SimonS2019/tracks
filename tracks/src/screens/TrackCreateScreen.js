880180fdc3431ed42384dbd40d51f5b06b36b3a4
SimonS2019 <shentiexiong@gmail.com>
2024-07-26 13:25:09 +1200

    232. Working on Track Create

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.h2}>Create a Track</Text>
+      <Map />
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  h2: {
+    fontSize: 48,
+  },
+});
 
 export default TrackCreateScreen;
